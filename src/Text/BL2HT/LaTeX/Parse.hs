0f05ccfe7504b6592418ea461b910c4b9ed45f22
Jeroen Janssen <jejansse@gmail.com>
2011-08-13 10:42:13 +0100

    Added recursive parser

--- src/Text/BL2HT/LaTeX/Parse.hs
+++ src/Text/BL2HT/LaTeX/Parse.hs
@@ -17,59 +17,88 @@ Parsing (some) LaTeX source code
 
 module Text.BL2HT.LaTeX.Parse (
       BasicTex(..)
-    , latexDocument
+    , parseDoc
 ) where
 
 import Text.ParserCombinators.Parsec hiding (many, (<|>))
 import Control.Applicative
 
-
 -- | Parses LaTeX command, returns (name, star, list of options or arguments).
-command :: GenParser Char st (String, [BasicTex])
-command = do
+parseCommand :: GenParser Char st BasicTex
+parseCommand = do
   char '\\'
   name <- many1 letter
-  args <- commandArgs
-  return (name, args)
+  optArgs <- parseOptArgs
+  reqArgs <- parseReqArgs
+  return $ Command name optArgs reqArgs
 
--- | Returns text between brackets and its matching pair.
-bracketedText :: Char -> Char -> GenParser Char st String
-bracketedText openB closeB = do
-  result <- charsInBalanced' openB closeB
-  return $ [openB] ++ result ++ [closeB]
+parseText :: GenParser Char st BasicTex
+parseText = OtherText <$> noCommand
+  where noCommand = many1 $ noneOf "\\"
 
--- | Returns an option or argument of a LaTeX command.
-optOrArg :: GenParser Char st BasicTex
-optOrArg = try $ spaces >> (OtherText <$> bracketedText '{' '}' <|> OtherText <$> bracketedText '[' ']')
+-- | Parses optional commands argument
+parseOptArgs :: GenParser Char st [BasicTex]
+parseOptArgs = try parseOptArg <|> return []
+  where 
+    parseOptArg = do
+      char '{'
+      arguments <- parseDoc
+      char '}'
+      return arguments
 
--- | True if the string begins with '{'.
-isArg :: String -> Bool
-isArg ('{':_) = True
-isArg _       = False
+parseReqArgs :: GenParser Char st [BasicTex]
+parseReqArgs = try parseReqArg <|> return []
+  where
+    parseReqArg = do
+      char '['
+      arguments <- parseDoc
+      char ']'
+      return arguments
 
--- | Returns list of options and arguments of a LaTeX command.
-commandArgs :: GenParser Char st [BasicTex]
-commandArgs = many optOrArg
+parseDoc :: GenParser Char st [BasicTex]
+parseDoc = many $ parseCommand <|> parseText
 
--- | Like @charsInBalanced@, but allow blank lines in the content.
-charsInBalanced' :: Char -> Char -> GenParser Char st String
-charsInBalanced' open close = try $ do
-  char open
-  raw <- many $       (many1 (satisfy $ \c -> c /= open && c /= close))
-                  <|> (do res <- charsInBalanced' open close
-                          return $ [open] ++ res ++ [close])
-  char close
-  return $ concat raw
-
+---- | Returns text between brackets and its matching pair.
+--bracketedText :: Char -> Char -> GenParser Char st String
+--bracketedText openB closeB = do
+--  result <- charsInBalanced' openB closeB
+--  return $ [openB] ++ result ++ [closeB]
+--
+---- | Returns an option or argument of a LaTeX command.
+--optOrArg :: GenParser Char st BasicTex
+--optOrArg = try $ spaces >> (OtherText <$> bracketedText '{' '}' <|> OtherText <$> bracketedText '[' ']')
+--
+---- | True if the string begins with '{'.
+--isArg :: String -> Bool
+--isArg ('{':_) = True
+--isArg _       = False
+--
+---- | Returns list of options and arguments of a LaTeX command.
+--commandArgs :: GenParser Char st [BasicTex]
+--commandArgs = many optOrArg
+--
+---- | Like @charsInBalanced@, but allow blank lines in the content.
+--charsInBalanced' :: Char -> Char -> GenParser Char st String
+--charsInBalanced' open close = try $ do
+--  char open
+--  raw <- many $       (many1 (satisfy $ \c -> c /= open && c /= close))
+--                  <|> (do res <- charsInBalanced' open close
+--                          return $ [open] ++ res ++ [close])
+--  char close
+--  return $ concat raw
+--
 data BasicTex =
-      Command   { texCommand :: (String, [BasicTex]) }
+      Command   { cmdName :: String
+                , optArgs :: [BasicTex]
+                , reqArgs :: [BasicTex]
+                }
     | OtherText { texText :: String }
     deriving Show
 
 -- | Parse a TeX document
-latexDocument = many token
-    where
-        token = Command   <$> command
-            <|> OtherText <$> nocommand
-        nocommand = many1 $ noneOf "\\"
+--latexDocument = many token
+--    where
+--        token = Command   <$> parseCommand
+--            <|> OtherText <$> nocommand
+--        nocommand = many1 $ noneOf "\\"
 
